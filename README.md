7c785bfc9500df69560c1bb6a113ce645518ef78
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2023-07-18 23:26:49 +0300

    Couple adjustments
    - Add vite copy plugin
    - Add dummy data
    - fix debugging in the console and header

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-
-
-<https://github.com/twigjs/twig.js/wiki/Extending-twig.js#functions>
-<https://vituum.dev/plugins/>
-<https://iconify.design/>
+# README
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import vituum from 'vituum';
 import twig from '@vituum/vite-plugin-twig';
+import cp from 'vite-plugin-cp';
+
 
 export default {
 	plugins: [
@@ -11,13 +13,26 @@ export default {
 				},
 			},
 		}),
+		
 		twig({
 			root: './src',
 			globals: {foo: 'bar'},
             functions: {
                     join: (...args) => args.join(''),
+					json_encode: (data) => JSON.stringify(data),
             }
 		}),
+		cp({
+			targets: [
+			  {src: './src/{footer,header,layout}.twig', dest: './dist'},
+			  {src: './src/common', dest: './dist/common'},
+			  {src: './src/templateS', dest: './dist/templateS'},
+			  {src: './src/locals', dest: './dist/locals'},
+			  {src: './src/modules', dest: './dist/modules'},
+			  {src: './src/assets',dest: './dist/assets'},
+
+			]
+		  })
 	],
 
 	base: '/',
